246f1743689a1233d564d7522dccb811a6c0c023
ItsBuddha <30822729-ItsBuddha@users.noreply.replit.com>
2025-02-04 23:30:56 +0000

    User checkpoint: Add wallets table and refactor balance field to include ETH and WEI values.

--- migrations/0000_absent_jackpot.sql
+++ migrations/0000_absent_jackpot.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE IF NOT EXISTS "chats" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
@@ -15,8 +14,19 @@ CREATE TABLE IF NOT EXISTS "messages" (
 	"metadata" json
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "wallets" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"address" text NOT NULL,
+	"network_id" text NOT NULL,
+	"balance_eth" text,
+	"balance_wei" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"last_updated" timestamp DEFAULT now() NOT NULL,
+	"wallet_data" json
+);
+--> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "messages" ADD CONSTRAINT "messages_chat_id_chats_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chats"("id") ON DELETE CASCADE ON UPDATE no action;
+ ALTER TABLE "messages" ADD CONSTRAINT "messages_chat_id_chats_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chats"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
